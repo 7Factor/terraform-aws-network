a8b5954f68f36660e245d249a78a1f2be5b7af7c
chris-malloy <chrismalloymusic@gmail.com>
2019-07-10 07:59:52 -0400

    Converting to HCL2 expressions.

--- output.tf
+++ output.tf
@@ -1,39 +1,39 @@
 output "utility_hosts_sg" {
-  value       = "${aws_security_group.utility_hosts.id}"
+  value       = aws_security_group.utility_hosts.id
   description = "The SG that utility boxes are assigned enabling SSH."
 }
 
 output "allow_utility_access_sg" {
-  value       = "${aws_security_group.allow_utility_access.id}"
+  value       = aws_security_group.allow_utility_access.id
   description = "Assign this SG to boxes you wish to be accessible from utilities."
 }
 
 output "public_subnets" {
-  value       = "${aws_subnet.public_subnets.*.id}"
+  value       = aws_subnet.public_subnets.*.id
   description = "Public subnet IDs configured with a cooresponding private subnet."
 }
 
 output "private_subnets" {
-  value       = "${aws_subnet.private_subnets.*.id}"
+  value       = aws_subnet.private_subnets.*.id
   description = "Private subnet IDs configured with a corresponding public subnet."
 }
 
 output "addl_private_subnets" {
-  value       = "${aws_subnet.addl_private_subnets.*.id}"
+  value       = aws_subnet.addl_private_subnets.*.id
   description = "Subnets configured with no public pairs. This doesn't mean they don't have a corresponding public subnet."
 }
 
 output "basion_host_public_ips" {
-  value       = "${aws_instance.bastion_hosts.*.public_ip}"
+  value       = aws_instance.bastion_hosts.*.public_ip
   description = "A list of public IP addresses for your bastion hosts."
 }
 
 output "basion_host_private_ips" {
-  value       = "${aws_instance.bastion_hosts.*.private_ip}"
+  value       = aws_instance.bastion_hosts.*.private_ip
   description = "A list of pribate IP addresses for your bastion hosts."
 }
 
 output "vpc_id" {
-  value       = "${aws_vpc.primary_vpc.id}"
+  value       = aws_vpc.primary_vpc.id
   description = "The ID of the primary AWS VPC."
 }
--- routes.tf
+++ routes.tf
@@ -1,6 +1,6 @@
 # Build out the route tables before we associate routes with them.
 resource "aws_route_table" "public_route_table" {
-  vpc_id     = "${aws_vpc.primary_vpc.id}"
+  vpc_id     = aws_vpc.primary_vpc.id
   depends_on = ["aws_internet_gateway.igw"]
 
   tags = {
@@ -9,7 +9,7 @@ resource "aws_route_table" "public_route_table" {
 }
 
 resource "aws_route_table" "private_route_table" {
-  vpc_id     = "${aws_vpc.primary_vpc.id}"
+  vpc_id     = aws_vpc.primary_vpc.id
   depends_on = ["aws_nat_gateway.nat_gw"]
 
   tags = {
@@ -19,37 +19,37 @@ resource "aws_route_table" "private_route_table" {
 
 # Add an iGW to the public route table
 resource "aws_route" "public_to_igw" {
-  route_table_id         = "${aws_route_table.public_route_table.id}"
+  route_table_id         = aws_route_table.public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.igw.id}"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 # Add the NAT gateway to the private route table
 resource "aws_route" "private_subnets_to_nat" {
-  route_table_id         = "${aws_route_table.private_route_table.id}"
+  route_table_id         = aws_route_table.private_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.nat_gw.id}"
+  nat_gateway_id         = aws_nat_gateway.nat_gw.id
 }
 
 # Associate all public subnets with the public routing table. There's a
 # hideous hack here because terraform cannot count computed lists. We
 # instead use the total number of public subnets defined in vars.
 resource "aws_route_table_association" "public_subnet_routes" {
-  count          = "${length(var.public_private_subnet_pairs)}"
-  route_table_id = "${aws_route_table.public_route_table.id}"
-  subnet_id      = "${element(aws_subnet.public_subnets.*.id, count.index)}"
+  count          = length(var.public_private_subnet_pairs)
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
   depends_on     = ["aws_route_table.public_route_table"]
 }
 
 # Let's also add the utility subnet to the public routes.
 resource "aws_route_table_association" "utility_subnet_routes" {
-  route_table_id = "${aws_route_table.public_route_table.id}"
-  subnet_id      = "${aws_subnet.utility_subnet.id}"
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.utility_subnet.id
   depends_on     = ["aws_route_table.public_route_table"]
 }
 
 # Set main route table to private
 resource "aws_main_route_table_association" "main_route" {
-  vpc_id         = "${aws_vpc.primary_vpc.id}"
-  route_table_id = "${aws_route_table.private_route_table.id}"
+  vpc_id         = aws_vpc.primary_vpc.id
+  route_table_id = aws_route_table.private_route_table.id
 }
