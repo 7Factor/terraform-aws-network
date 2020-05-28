075ed036d7693c1eb84b2acd9553548aa473df44
chris-malloy <chrismalloymusic@gmail.com>
2020-05-28 18:12:19 -0400

    Testing cidr blocks.

--- integration/spec/main_spec.rb
+++ integration/spec/main_spec.rb
@@ -8,4 +8,12 @@ describe vpc(ENVVARS[:vpc_id][:value]) do
   it { should exist }
   it { should be_available }
   it { should have_tag('Name').value('Primary VPC') }
+
+  it 'should have the correct cidr blocks' do
+    cidr_blocks = subject.cidr_block_association_set.map {|cidr_block| cidr_block}
+
+    expect(cidr_blocks[0].cidr_block).to eq TFVARS['vpc_primary_cidr']
+    expect(cidr_blocks[1].cidr_block).to eq TFVARS['vpc_addl_address_space'][0]
+    expect(cidr_blocks[2].cidr_block).to eq TFVARS['utility_subnet_cidr']
+  end
 end
