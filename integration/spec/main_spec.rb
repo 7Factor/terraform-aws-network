bd76d4b849922467274d2b54e40a5b2a710deefe
chris-malloy <chrismalloymusic@gmail.com>
2020-05-28 17:15:08 -0400

    Writing vpc tests.

--- integration/spec/main_spec.rb
+++ integration/spec/main_spec.rb
@@ -2,3 +2,10 @@ require 'awspec'
 require 'hcl/checker'
 
 TFVARS = HCL::Checker.parse(File.open('testing.tfvars').read())
+ENVVARS = eval(ENV['KITCHEN_KITCHEN_TERRAFORM_OUTPUTS'])
+
+describe vpc(ENVVARS[:vpc_id][:value]) do
+  it { should exist }
+  it { should be_available }
+  it { should have_tag('Name').value('Primary VPC') }
+end
